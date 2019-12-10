67d60bc71ecc050fb3780cd8a39f95ed303e4268
Anna Lewis Dagnino <annaick1995@gmail.com>
2019-12-10 00:57:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(draves_names)
-  draves_names.each_with_index{|name, index| puts "#{index} #{name}"}
+  draves_names.each_with_index{|name, index| puts "#{index+1} #{name}"}
 end
 
 def summon_captain_planet# code an argument here
